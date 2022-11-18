aa27151c056ae6209b4b8361b4e83441f736ecf0
Rohan Tiwari <ping.techyrohan@gmail.com>
2022-11-18 15:48:36 +0545

    final

--- app.js
+++ app.js
@@ -5,11 +5,25 @@ const userRouter = require('./routes/userRoute');
 const postRouter = require('./routes/postRoute');
 const globalErrorHandler = require('./utility/globalErrorHandler');
 const AppError = require('./utility/AppError');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const sanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 // environment variables configuration
 dotenv.config();
 
+const limiter = rateLimit({
+    max: 20,
+    windowMs: 5 * 60 * 1000,
+    message: 'Too Many Request Try Again Later',
+});
+
 // middlewares
+app.use(sanitize());
+app.use(xss);
+app.use(helmet());
+app.use(limiter);
 app.use(express.json());
 app.use('/api/v1/post', postRouter);
 app.use('/api/v1', userRouter);
